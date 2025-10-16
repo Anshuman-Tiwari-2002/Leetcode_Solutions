25da1fd2a59c146a339028ceb35df58ba8c1bc25
ANSHUMAN TIWARI <anshumant111@gmail.com>
2025-10-16 18:16:40 +0530

    Time: 50 ms (92.05%), Space: 58.3 MB (15.76%) - LeetHub

---
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var hasIncreasingSubarrays = function(nums, k) {
+    let increaseCount = 1, prev = 0;
+
+    for(let i=1; i<=nums.length; i++) {
+        if(nums[i-1]< nums[i])
+        increaseCount++;
+        else{
+            prev = increaseCount;
+            increaseCount = 1;
+        }
+        if(Math.floor(increaseCount / 2) >= k || Math.floor(prev / 2) >=    k || Math.min(prev, increaseCount) >= k)
+        return true;
+    }
+    return false;
+};
